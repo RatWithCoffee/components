ae8ca06898e2f04f06b6d3478fa44797a57b65e0
ratwithcoffee <mashaglotova2003@gmail.com>
2023-09-30 10:35:34 +0300

    fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import { HeaderTop } from './HeaderTop';
-import { HeaderBottom } from './HeaderBottom';
+import { ArrowSvg, AuthorSvg, TimeSvg } from '../svg';
+import { BranchesButton } from './capsules/BranchesButton';
+import { CommitsButton } from './capsules/CommitsButton';
+import { ParentCommit } from './capsules/ParentCommit';
+import React, { useState } from 'react'
 
-export const Header = ({ branches, commits, parents,  }) => {
-    return (<div>
-        <HeaderTop/>
-        <HeaderBottom/>
+export const Header = ({ title, author, time, branches, commits, parents, ...props }) => {
+    const [branch, setBranch] = useState(branches[0]);
+    const [commit, setCommit] = useState(commits[0]);
+
+
+    const handleSelectBranch = (value) => () => {
+        setBranch(value);
+    }
+
+    const handleSelectCommit = (value) => () => {
+        setCommit(value);
+    }
+
+    return (<div className='header'>
+        <div className='header_top'>
+            <div className='header_commit-msg'>{title}</div>
+            <div className='header_commit-author'>
+                <div className='icon-type'>
+                    <AuthorSvg />
+                </div>
+                <span>{author}</span>
+            </div>
+            <div className='header_commit-time'>
+                <div className='icon-type'>
+                    <TimeSvg />
+                </div>
+                <span>{time}</span>
+            </div>
+        </div>
+
+        <div className='header_bottom'>
+            <BranchesButton branch={branch} handleSelectBranch={handleSelectBranch} />
+            {parents.map((e) =>
+                <ParentCommit commit={e} />
+            )}
+            <div className='icon-arrow'>
+                <ArrowSvg />
+            </div>
+            <CommitsButton commit={commit} handleSelectCommit={handleSelectCommit} />
+        </div>
+
     </div>);
 }
